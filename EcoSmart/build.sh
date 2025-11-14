dfa0538995fedb15842dbb69ab05e28ab8e8e7b4
cazifu <liolioclem@gmail.com>
2025-11-14 10:21:33 -0300

    ahora sii creo

--- EcoSmart/build.sh
+++ EcoSmart/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
-cd EcoSmart
 python manage.py collectstatic --no-input
 python manage.py migrate
